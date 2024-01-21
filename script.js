4eb8841a052d40c9f9e44c9c710012e0151ad559
ZeonAnimations <89378477+ZeonAnimations@users.noreply.github.com>
2024-01-21 20:37:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript: Change header color on click
+const header = document.querySelector('header');
+header.addEventListener('click', function() {
+  header.style.backgroundColor = '#ff8c00';
+});
